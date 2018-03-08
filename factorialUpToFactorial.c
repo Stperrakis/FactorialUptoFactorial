d0b1f85035a4a34de5222fa5369a898cb18dc8cf
Perrakis Stylianos <stelperr@gmail.com>
2018-03-08 03:30:45 +0200

    Update factorialUpToFactorial.c

--- factorialUpToFactorial.c
+++ factorialUpToFactorial.c
@@ -4,16 +4,14 @@
 int main(int argc, char argv) {
     int i,j=-1,k,z;
     float res,t;
-
     t = 1;
-
     while(j<0) {
 
         printf("Give me a valid number(positive)");
         scanf("%d",&j);
          }
     if (j==0) {
-        printf("Your result is 0");
+       exit (-1);
     } else {
         for (k=1; k<j+1; k++) {
             t=t*k;
